db6c7c9f78fe92356d7f07558beeefd9e2a27407
zven21 <andzven@gmail.com>
2019-06-10 14:20:14 +0800

    prepare to publish.

--- mix.exs
+++ mix.exs
@@ -1,12 +1,19 @@
 defmodule Aasm.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @github "https://github.com/zven21/aasm"
+
   def project do
     [
       app: :aasm,
-      version: "0.1.0",
+      version: @version,
+      description: "The finite state machine implementations for Elixir.",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      # aliases: aliases(),
+      package: package(),
+      elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps()
     ]
   end
@@ -21,8 +28,32 @@ defmodule Aasm.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["zven21"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @github}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "priv", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # defp aliases do
+  #   [
+  #     test: [
+  #       "ecto.drop --quiet",
+  #       "ecto.create --quiet",
+  #       "ecto.migrate --quiet",
+  #       "test"
+  #     ]
+  #   ]
+  # end
 end
